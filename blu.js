ebb82e710b7e4620995fae9277a76af73d1eb69b
BlueeeAi <honeylovemempin65@gmail.com>
2024-11-07 12:49:12 +0800

    Update and rename commands/gpt4.js to blu.js

--- blu.js
+++ blu.js
@@ -2,20 +2,22 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-  name: 'gpt4',
-  description: 'Interact with GPT-4o',
-  usage: 'gpt4 [your message]',
-  author: 'coffee',
+  name: 'blu',
+  description: 'Interact with Blu AI',
+  usage: 'blu [your message]',
+  author: 'Blu',
 
   async execute(senderId, args, pageAccessToken) {
-    const prompt = args.join(' ');
-    if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
+    const prompt = args.join(' ').toLowerCase();
+
+    if (!prompt) return sendMessage(senderId, { text: "Usage: blu <question>" }, pageAccessToken);
 
     try {
       const { data: { result } } = await axios.get(`https://joshweb.click/api/gpt-4o?q=${encodeURIComponent(prompt)}&uid=${senderId}`);
       sendMessage(senderId, { text: result }, pageAccessToken);
-    } catch {
+    } catch (error) {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
   }
 };
+
